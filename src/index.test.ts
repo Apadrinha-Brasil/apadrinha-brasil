85eb989d40714fd6501e01b91075249882bb501f
Bruno Lima <brunolima.cad@gmail.com>
2023-11-10 02:20:59 -0700

    completed todo

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,6 @@
+// TODO: Deletar esse arquivo jest quando testes reais forem adicionados
+// O interesse desse teste foi para o suporte do flow da pipeline CI
 describe('Placeholder test', () => {
-  // TODO: 
   it ('to support the CI pipeline flow', () => {
     expect(true).toBe(true)
   })

082f747e8095e9f593a2373e383ee4983b4a25e1
viparis <xxvi.paris@gmail.com>
2023-11-17 17:12:22 -0300

    feat: create select structure

--- src/client/components/MainForm.tsx
+++ src/client/components/MainForm.tsx
@@ -2,20 +2,36 @@ import React from 'react'
 import { useFormik } from 'formik'
 import { Input } from '.'
 
-
 export const MainForm = () => {
   const formik = useFormik({
     initialValues: {
       email: '',
+      selecionarOpcao: '',
     },
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2))
     },
   })
+
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <Input name="email" type="email" value={formik.values.email} handleChange={formik.handleChange}/>
+      <label htmlFor="email">Endereço de Email:</label>
+      <Input name="email" type="email" value={formik.values.email} handleChange={formik.handleChange} />
+      
+      <label htmlFor="selecionarOpcao">Incrível Select:</label>
+      <select
+        id="selecionarOpcao"
+        name="selecionarOpcao"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.selecionarOpcao}
+      >
+        <option value="" label="Selecione..." />
+        <option value="opcao1" label="Opção 1" />
+        <option value="opcao2" label="Opção 2" />
+        <option value="opcao3" label="Opção 3" />
+      </select>
+      
       <button type="submit">Submit</button>
     </form>
   )

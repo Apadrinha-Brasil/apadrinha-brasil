7c0ed51187d5171d4072a9d5a6ee2aaa1eaa03d9
viparis <xxvi.paris@gmail.com>
2023-11-19 10:57:00 -0300

    refactor: change useFormik hook to Formik component

--- src/client/components/MainForm.tsx
+++ src/client/components/MainForm.tsx
@@ -1,38 +1,41 @@
 import React from 'react'
-import { useFormik } from 'formik'
+import { Formik, Field, Form } from 'formik'
 import { Input } from '.'
 
 export const MainForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      selecionarOpcao: '',
-    },
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2))
-    },
-  })
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Endereço de Email:</label>
-      <Input name="email" type="email" value={formik.values.email} handleChange={formik.handleChange} />
-      
-      <label htmlFor="selecionarOpcao">Incrível Select:</label>
-      <select
-        id="selecionarOpcao"
-        name="selecionarOpcao"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.selecionarOpcao}
-      >
-        <option value="" label="Selecione..." />
-        <option value="opcao1" label="Opção 1" />
-        <option value="opcao2" label="Opção 2" />
-        <option value="opcao3" label="Opção 3" />
-      </select>
-      
-      <button type="submit">Submit</button>
-    </form>
+    <Formik
+      initialValues={{
+        email: '',
+        selecionarOpcao: '',
+      }}
+      onSubmit={(values) => {
+        alert(JSON.stringify(values, null, 2))
+      }}
+    >
+      <Form>
+        <label htmlFor="email">Endereço de Email:</label>
+        <Field
+          type="email"
+          id="email"
+          name="email"
+          component={Input}
+        />
+
+        <label htmlFor="selecionarOpcao">Incrível Select:</label>
+        <Field
+          as="select"
+          id="selecionarOpcao"
+          name="selecionarOpcao"
+        >
+          <option value="" label="Selecione..." />
+          <option value="opcao1" label="Opção 1" />
+          <option value="opcao2" label="Opção 2" />
+          <option value="opcao3" label="Opção 3" />
+        </Field>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   )
 }

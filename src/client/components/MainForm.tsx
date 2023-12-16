dffe0bab42073df6848a5c077ffb280f8010446d
viparis <xxvi.paris@gmail.com>
2023-12-16 08:19:48 -0300

    style: add css

--- src/client/components/MainForm.tsx
+++ src/client/components/MainForm.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import { Select, CheckboxGroup, RadioGroup, Button } from '.'
 import * as yup from 'yup'
 
 export const MainForm = () => {
+  const [emailFocus, setEmailFocus] = useState(false)
+  const [selectFocus, setSelectFocus] = useState(false)
+  const [checkboxFocus, setCheckboxFocus] = useState(false)
+  const [radioFocus, setRadioFocus] = useState(false)
+
   const validationSchema = yup.object().shape({
     email: yup.string().email('Digite um e-mail válido').required('O e-mail é obrigatório'),
     selectOption: yup.string().required('O campo é obrigatório'),
@@ -44,25 +49,34 @@ export const MainForm = () => {
         validationSchema={validationSchema}
       >
         <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Endereço de Email:</label>
-          <Field className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline max-w-1/2" type="email" id="email" name="email" />
-          <ErrorMessage className='error text-red-700' name="email" component="div" />
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Endereço de Email:
+          </label>
+          <Field
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline max-w-1/2 ${
+              emailFocus ? 'focus:border-blue-500 focus:ring-blue-500' : ''
+            }`}
+            type="email"
+            id="email"
+            name="email"
+            onFocus={() => setEmailFocus(true)}
+            onBlur={() => setEmailFocus(false)}
+          />
+          <ErrorMessage className="error text-red-700" name="email" component="div" />
 
-          <div className="w-full px-3 mb-6 md:mb-0">
+          <div className="w-full px-3 mb-6 md:mb-0 border-black">
             <Field
               name="selectOption"
               label="Incrível Select:"
               options={optionsSelect}
               component={Select}
-              className="peer p-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
-              focus:pt-6
-              focus:pb-2
-              [&:not(:placeholder-shown)]:pt-6
-              [&:not(:placeholder-shown)]:pb-2
-              autofill:pt-6
-              autofill:pb-2"
+              className={`peer p-4 pe-9 block w-full border-black rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 ${
+                selectFocus ? 'focus:pt-6 focus:pb-2' : ''
+              }`}
+              onFocus={() => setSelectFocus(true)}
+              onBlur={() => setSelectFocus(false)}
             />
-            <ErrorMessage className='error text-red-700' name="selectOption" component="div" />
+            <ErrorMessage className="error text-red-700" name="selectOption" component="div" />
           </div>
 
           <Field
@@ -70,18 +84,22 @@ export const MainForm = () => {
             label="Escolha as opções:"
             options={optionsCheckbox}
             component={CheckboxGroup}
-            className="mt-4"
+            className={`mt-4 ${checkboxFocus ? 'focus' : ''}`}
+            onFocus={() => setCheckboxFocus(true)}
+            onBlur={() => setCheckboxFocus(false)}
           />
-          <ErrorMessage className='error text-red-700' name="checkboxes" component="div" />
+          <ErrorMessage className="error text-red-700" name="checkboxes" component="div" />
 
           <Field
             name="radioOption"
             label="Escolha uma opção:"
             options={optionsRadio}
             component={RadioGroup}
-            className="mt-4"
+            className={`mt-4 ${radioFocus ? 'focus' : ''}`}
+            onFocus={() => setRadioFocus(true)}
+            onBlur={() => setRadioFocus(false)}
           />
-          <ErrorMessage className='error text-red-700' name="radioOption" component="div" />
+          <ErrorMessage className="error text-red-700" name="radioOption" component="div" />
 
           <Button type="submit" label="Submit" />
         </Form>
